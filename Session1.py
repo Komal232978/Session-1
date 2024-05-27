70be13720d432faecc531c18b8bc40f73f8ae082
Komal232978 <232978@students.aih.nsw.edu.au>
2024-05-27 18:46:22 +1000

    Create Session1.py
    This is regarding basic knowledge of pseudocode..

---
+++ Session1.py
@@ -0,0 +1,22 @@
+# write a program to input age of a user and let them know if they have diabete 
+# risk or not. Use case is that a user with age more than 65 is risky else not
+# risky
+
+Age=55
+if age>=65
+print("This is risky")
+else
+print("not risky")
+
+# Write a program to calculate the GST tsx of two products, apple and pie
+# apple has 10% GST and pie has only 8%
+# calculate total GST that need to be deducted from the bill
+# apple gross sale is $75, pie gross is $100
+
+apple = 75
+pie = 100
+gst_apple = 0.1*apple
+gst_pioe = 0.08*pie
+
+print(gst_apple)
+print(gst_pie)
